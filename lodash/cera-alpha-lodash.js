91f04b11a190ec441c1bcc625a74b4b93b87e38c
Cera-alpha <38802745+Cera-alpha@users.noreply.github.com>
2018-07-12 14:59:56 +0800

    Update cera-alpha-lodash.js

--- lodash/cera-alpha-lodash.js
+++ lodash/cera-alpha-lodash.js
@@ -1,5 +1,9 @@
 var cera_alpha = {
-  chunk: function(array, size) {
-
-  }
+  chunk: function(array, size = 1) {
+    var result = [];
+    for (var i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, size + i));
+    }
+    return result;
+    },
 }
